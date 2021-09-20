1e016cbb6035c4e15e366e0d0e4ffc6cc0713b58
Yuliya Prach <yuliya_prach@epam.com>
2021-09-20 17:33:04 +0300

    Initial setup of webpack and project

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+
+    },
+    mode: 'development',
+    devServer: {
+        open: true,
+        compress: true,
+        port: 8000,
+    },
+}
